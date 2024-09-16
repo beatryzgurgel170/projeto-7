8b58bc4e53bde7355c93fff49fb41053660622bc
beatryzgurgel170 <anna.gurgel1@aluno.ce.gov.br>
2024-09-16 16:15:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = prompt(`Escolha uma forma de pagamento: 
+    1 - opção 1
+    2 - opção 2
+    3 - opção 3
+    4 - opção 4
+    5 - opção 5`)
+    do{
+      switch(menu){
+        case 1:
+          alert('Você escolheu opção 1')
+          break
+        case 2:
+          alert('Você escolheu opção 2')
+          break
+        case 3:
+          alert('Você escolheu opção 3')
+          break
+        case 4:
+          alert('Você escolheu opção 4')
+          break
+      }
+    } while (menu != 5)
